bddde21b76cf6bc72eb3a17516fe26799a59819f
Imalka Gayani Fernando <101160334+isneha44@users.noreply.github.com>
2024-04-29 14:48:34 +0530

    Project Initialized

---
+++ backend/src/test/java/lk/ijse/helloshoeshopmanagement/HelloShoeShopManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloshoeshopmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShoeShopManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
