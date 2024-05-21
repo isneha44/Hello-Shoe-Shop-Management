7a0e6f73d375c03d9c2584ee387744bb5b257a8c
Imalka Gayani Fernando <101160334+isneha44@users.noreply.github.com>
2024-05-21 12:37:43 +0530

    edited frontend and added getAllCustomer Ficntion in customer controller backend

--- backend/src/main/java/lk/ijse/helloshoeshopmanagement/controller/CustomerController.java
+++ backend/src/main/java/lk/ijse/helloshoeshopmanagement/controller/CustomerController.java
@@ -9,14 +9,12 @@ import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -75,6 +73,17 @@ public class CustomerController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         } finally {
             loggerLog4J.info("End saveCustomer");
+        }
+    }
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomer()  {
+        loggerLog4J.info("Start getAllCustomer");
+        try {
+            loggerLog4J.info("End getAllItem");
+            return ResponseEntity.ok(customerService.getAllCustomer());
+        } catch (Exception e) {
+            handleException(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
