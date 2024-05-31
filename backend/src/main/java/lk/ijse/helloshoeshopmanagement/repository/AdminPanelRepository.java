2d74dfda9e828f661d2fced5076cc246f818648e
Imalka Gayani Fernando <101160334+isneha44@users.noreply.github.com>
2024-05-31 11:25:09 +0530

    CreatedAdminPanel Function

--- backend/src/main/java/lk/ijse/helloshoeshopmanagement/repository/AdminPanelRepository.java
+++ backend/src/main/java/lk/ijse/helloshoeshopmanagement/repository/AdminPanelRepository.java
@@ -1,6 +1,12 @@
 package lk.ijse.helloshoeshopmanagement.repository;
+
+import lk.ijse.helloshoeshopmanagement.entity.AdminPanel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
 /**
  * @author Imalka Gayani
  */
-public interface AdminPanelRepository {
+public interface AdminPanelRepository extends JpaRepository<AdminPanel, UUID> {
 }
